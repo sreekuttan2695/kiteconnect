533ca1ca10b88cacb651940b7eff75132d6856f1
Sreekesh <sreekuttan2695@gmail.com>
2024-06-13 22:54:35 +0530

    Selenium added with warning

---
+++ src/main/java/stock/eclerne/kiteconnect2/SeleniumTest.java
@@ -0,0 +1,22 @@
+package stock.eclerne.kiteconnect2;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\Code\\kiteconnect2\\drivers\\chromedriver.exe");
+
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://www.example.com");
+
+        // Perform actions on the website
+        System.out.println("Title: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
+}
